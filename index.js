d4866305dca22fb74c69fb0a55e701c4e92dcc01
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-27 16:29:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function BoardMember (name, homeState, training) {
   this.name = name; 
   this.homeState = homeState; 
-  this 
+  this. training = training; 
+  
   this.veto = function(){
     return 'No, I must disagree';
   }
