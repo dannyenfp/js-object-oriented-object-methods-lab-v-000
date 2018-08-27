32941f24a2d8dcb401d292120350b418440a42e1
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-27 16:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ function BoardMember(name, homeState, training) {
   this.veto = function(){
     return 'No, I must disagree';
   }
-  this.approve = functin 
+  this.approve = function(){ 
+    return "You can do that!";
+  }
+  this.doCharity = function(){ 
+    return "I like to help people.";
+  }
+  this.releasePressStatement = functon(){ 
+    return "You will see great things from Scuber";
+  }
+  this.sayHi = function(){
+    return `Hi, my name is 
+  }
 }
